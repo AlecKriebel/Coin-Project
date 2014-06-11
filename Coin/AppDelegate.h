08b18310afbd800ce4f17f2564a3782f33083d08
Alec Kriebel <alec@instamour.com>
2014-06-11 12:19:20 -0400

    Initial Commit

---
+++ Coin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Coin
+//
+//  Created by Alec Kriebel on 6/11/14.
+//  Copyright (c) 2014 AlecKriebel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Coin/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Coin
+//
+//  Created by Alec Kriebel on 6/11/14.
+//  Copyright (c) 2014 AlecKriebel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
